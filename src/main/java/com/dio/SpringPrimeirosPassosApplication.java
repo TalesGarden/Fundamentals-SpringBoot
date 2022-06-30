6fff66ceee65d7932fa72f55185bb1e01863b8a2
TalesJardim <talesjardim@gmail.com>
2022-06-30 15:21:02 -0300

    VALUES PROPERTIES

--- src/main/java/com/dio/SpringPrimeirosPassosApplication.java
+++ src/main/java/com/dio/SpringPrimeirosPassosApplication.java
@@ -3,51 +3,68 @@ package com.dio;
 import com.dio.service.CarVehicleService;
 import com.dio.service.VehicleService;
 import com.dio.service.VehicleTestService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+												//
 public class SpringPrimeirosPassosApplication {
 
 	public static void main(String[] args) {
 
 		ApplicationContext apc =  SpringApplication.run(SpringPrimeirosPassosApplication.class, args);
-
-		//mostra todos os Beans da aplicação
-		for (String element : apc.getBeanDefinitionNames()){
-			System.out.println(element);
-		}
-
-		ConfigurableApplicationContext cac = SpringApplication.run(SpringPrimeirosPassosApplication.class, args);
-		Alien a = cac.getBean(Alien.class);
-		System.out.println("________________________________________________________");
-		a.setName("TALES");
-		a.setTech("JAVA");
-		a.show();
-		System.out.println("________________________________________________________");
-
-//		Alien b = cac.getBean(Alien.class);
-//		b.setName("ROBERTO");
-//		b.show();
+		System.out.println("HELLO FROM SPRINGAPPLICATION .RUN dentro do MAIN");
+//		//mostra todos os Beans da aplicação
+//		for (String element : apc.getBeanDefinitionNames()){
+//			System.out.println(element);
+//		}
+//
+//		ConfigurableApplicationContext cac = SpringApplication.run(SpringPrimeirosPassosApplication.class, args);
+//		Alien a = cac.getBean(Alien.class);
+//		System.out.println("________________________________________________________");
+//		a.setName("TALES");
+//		a.setTech("JAVA");
+//		a.show();
 //		System.out.println("________________________________________________________");
 //
+////		Alien b = cac.getBean(Alien.class);
+////		b.setName("ROBERTO");
+////		b.show();
+////		System.out.println("________________________________________________________");
+////
+//
+////		VehicleService vehicleService = cac.getBean(CarVehicleService.class);
+////		vehicleService.startEngine();
+////		vehicleService.move();
+////		vehicleService.stopEngine();
+//
+//		VehicleTestService vehicleTestService = cac.getBean(VehicleTestService.class);
+//		vehicleTestService.testVehicle();
 
-//		VehicleService vehicleService = cac.getBean(CarVehicleService.class);
-//		vehicleService.startEngine();
-//		vehicleService.move();
-//		vehicleService.stopEngine();
-
-		VehicleTestService vehicleTestService = cac.getBean(VehicleTestService.class);
-		vehicleTestService.testVehicle();
 
 
 	}
 
+	// public class SpringPrimeirosPassosApplication implements CommandLineRunner, ApplicationRunner
+
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.println("Hello from COmandLinerUNNER");
+//	}
+//
+//	@Override
+//	public void run(ApplicationArguments args) throws Exception {
+//		System.out.println("Hello from  ApplicationRunner IMPLEMENTATION ");
+//	}
 
 
-	}
+}
 
--- src/main/java/com/dio/app/SistemaMensagem.java
+++ src/main/java/com/dio/app/SistemaMensagem.java
@@ -1,18 +1,36 @@
 package com.dio.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-
+import org.springframework.stereotype.Component;
 import java.util.List;
 
+
+@Component
 public class SistemaMensagem implements CommandLineRunner {
 
-    private String nome;
-    private String email;
-    private List<Long> telefones;
+    @Autowired
+    private Remetente remetente;
+
+    // @Value busca no arquivo application.properties os valores de cada campo. Caso não encontre
+    // o atributo no arquivo, existe a possibilidade de um valor padrão ao realizar a anotação ex :
+    //     @Value("${name: Dio}"), observe que o atributo está escrito com "NAME" e não "Nome" logo o nome
+    // atribuido será DIO e não o valor encontrado no arquivo properties
+
+//    @Value("${remetente.nome: Dio}")
+//    private String nome;
+//    @Value("${remetente.email}")
+//    private String email;
+//    @Value("${remetente.telefones}")
+//    private List<Long> telefones;
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Mensagem enviada por " + this.nome + " Email: " + this.email + " telefone ");
+        System.out.println("Mensagem enviada por " + remetente.getNome()
+                + "\nEmail: " + remetente.getEmail() +
+                " \nCom telefones para contato "+ remetente.getTelefones());
+        System.out.println("SEU CADASTRO FOI APROVADO");
     }
 
 
